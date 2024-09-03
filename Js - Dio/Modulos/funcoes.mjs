99c476c541cd0f1930b75bae2e8a5873ffd50c14
linhareshian <hian.linhares@rethink.dev>
2024-09-02 22:09:08 -0300

    Módulos - Dio

---
+++ Js - Dio/Modulos/funcoes.mjs
@@ -0,0 +1,22 @@
+// Opção 1
+export function mostraIdade (nome, idade){
+    return ` A idade de ${nome} é ${idade}`
+}
+
+
+function mostraCidade (nome, cidade){
+    return ` A Cidade de ${nome} é ${cidade}`
+}
+
+
+function mostraHobby (nome, hobby){
+    return ` O hobby de  ${nome} é ${hobby}`
+}
+
+//Opção 2
+export{
+    mostraCidade,
+    mostraHobby,
+    mostraIdade
+}
+
---
+++ Js - Dio/Modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Hian', 23))
+console.log(mostraHobby('Hian', 'Jogar bola'))
+console.log(mostraCidade('Hian', 'Timóteo'))
+
