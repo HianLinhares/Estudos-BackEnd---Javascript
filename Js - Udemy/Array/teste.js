1c4f546818e3a1855eb1768b0a51294425326158
Hian Linhares <hianfernando@hotmail.com>
2024-11-11 23:17:06 -0300

    Alterações no submódulo Estudos_Javascript

---
+++ Js - Udemy/Array/teste.js
@@ -0,0 +1,19 @@
+const retornaNomeHian = {
+    nome: "Hian",
+    sobrenome: "Linhares"
+
+
+}
+
+
+console.log(retornaNomeHian)
+
+
+
+
+const helloWorld = () => {
+    return " Hello World "
+}
+
+
+console.log(helloWorld())
